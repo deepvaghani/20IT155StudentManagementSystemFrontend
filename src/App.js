4844f5550f7430edc4bdddf9527603632f011149
Deep Vaghani <20it155@charusat.edu.in>
2022-09-07 13:45:56 +0530

    Added Navigationbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import NavigationBar from './Components/NavigationBar';
 import Student from './Components/Student';
 import StudentList from './Components/StudentList';
 
 function App() {
   return (
     <div className="App">
+      <NavigationBar/>
       <Student/>
       <StudentList/>
     </div>
