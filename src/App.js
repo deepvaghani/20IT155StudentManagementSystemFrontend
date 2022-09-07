c82e6fd3d2d0bfbc4b76e92dcbd7e26db59197b3
Deep Vaghani <20it155@charusat.edu.in>
2022-09-07 13:57:11 +0530

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import NavigationBar from './Components/NavigationBar';
 import Student from './Components/Student';
 import StudentList from './Components/StudentList';
+import Footer from './Components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavigationBar/>
       <Student/>
       <StudentList/>
+      <Footer/>
     </div>
   );
 }
