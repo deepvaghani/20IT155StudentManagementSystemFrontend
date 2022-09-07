ff238e1c0d25fe16c4d3212d5558368e654703c5
Deep Vaghani <20it155@charusat.edu.in>
2022-09-07 18:10:50 +0530

    Added Delete.

--- src/Components/StudentList.js
+++ src/Components/StudentList.js
@@ -17,6 +17,17 @@ export default function StudentList() {
       .then((response) => setStudents(response.data))
       .catch((error) => alert(error));
   };
+
+  let deleteStudent = (studentId) => {
+    axios.delete("http://localhost:8080/student/"+studentId)
+    .then(response=> {
+      if (response.data !== null){
+        alert("Record Deleted Successfully");
+        
+      }
+    })
+  }
+
   return (
     <div className="my-3">
       <Container>
@@ -39,18 +50,18 @@ export default function StudentList() {
                   <td colSpan={3}>{students.length} Studnets Available!!!</td>
                 </tr>
               ) : (
-                students.map((student) =>
-                  <tr key={student.id}>
-                    <td>{student.id}</td>
-                    <td>{student.name}</td>
-                    <td>{student.address}</td>
-                    <td>
-                      <ButtonGroup>
-                        <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{' '}
-                        <Button size="sm" variant="outline-danger"><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
-                      </ButtonGroup>
-                    </td>
-                  </tr>
+                students.map((student)=>
+                <tr key={student.id}>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td>
+                    <ButtonGroup>
+                      <Button size="sm" variant="outline-primary"><FontAwesomeIcon icon={faEdit}> Edit </FontAwesomeIcon></Button>{ ' '}
+                      <Button size="sm" variant="outline-danger" onClick={deleteStudent.bind(this,student.id)}><FontAwesomeIcon icon={faTrash}> Delete </FontAwesomeIcon></Button>
+                    </ButtonGroup>
+                  </td>
+                </tr>
                 )
               )}
             </tbody>
